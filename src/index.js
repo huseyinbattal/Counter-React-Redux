9cdbaa00f09bd9b07b6d98cab822c21a83a49ac3
huseyinbatta <huseyinbattal42@gmail.com>
2022-03-09 15:51:11 +0300

    Counter App with Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import counterReducer from "./reducers/counterReducer"
+
+const store = createStore(counterReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+   </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
